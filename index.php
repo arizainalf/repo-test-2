7a6f4413eeaa170c19cd9ba8ff4c5d09ab2fb7f7
arizainalf <arizainalf@gmail.com>
2024-01-23 10:49:16 +0700

    Menambahkan Lorem

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, doloremque ex. Quis, eveniet est. Iusto eum quisquam architecto! Omnis cum quidem similique numquam officia impedit aut nam fugiat nostrum magni.</p>
 </body>
 </html>
