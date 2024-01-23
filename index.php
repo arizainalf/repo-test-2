a28e49f05c124edc005cf391c42172b9490f31cd
arizainalf <arizainalf@gmail.com>
2024-01-23 10:53:51 +0700

    Menambahkan Lorem Lagi

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, doloremque ex. Quis, eveniet est. Iusto eum quisquam architecto! Omnis cum quidem similique numquam officia impedit aut nam fugiat nostrum magni.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda odio iusto explicabo similique voluptates quidem totam impedit laboriosam blanditiis, perferendis commodi, harum eveniet quisquam quod fuga! Eligendi consequuntur impedit porro?</p>
 </body>
 </html>
